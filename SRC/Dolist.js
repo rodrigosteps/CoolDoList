5728313620065f9bdb9163df23e955af990a9852
RODRIGO PASSOS <rodrigo.ctto@gmail.com>
2025-10-25 18:10:08 -0300

    update

--- SRC/Dolist.js
+++ SRC/Dolist.js
@@ -1,11 +1,9 @@
 
 const AddNewTask = document.getElementById("AddTask");//Task const
-
-function NewTask(){//main function
-
 const getTask = document.getElementById("TaskInput");//take the user input 
 const NoTaskAlert = document.getElementById("NoTask")// If no task is add show an alert
 
+function NewTask(){//main function
 
 if (getTask.value !== "" ){ //Verify if input is empty
 
@@ -14,18 +12,21 @@ if (getTask.value !== "" ){ //Verify if input is empty
     document.getElementById('Tasklist').appendChild(TaskIn)
     TaskIn.textContent = getTask.value;
     getTask.value="";//clear input field after add new task
-
     NoTaskAlert.textContent="";//clear no task alert
 }
 
 else{
     NoTaskAlert.textContent="Add a New task!";// If no task is add show an alert
 }
-
-
 }
 
 //Trigger Event
 AddNewTask.addEventListener("click",NewTask);
 
-
+//Keyboard function
+getTask.addEventListener('keydown',function(event){
+    if(event.key === 'Enter'){
+        event.preventDefault();
+        NewTask();
+    }
+})
